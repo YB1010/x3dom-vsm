e70ed5974ad995c1cedf34ca7e99474e676955ae
pimsuwan <hapztron@gmail.com>
2013-02-05 23:01:13 +0800

    Added help page

---
+++ pages/help.page.php
@@ -0,0 +1,27 @@
+<div class="modal-header">
+	<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+	<h3 id="myModalLabel">Help</h3>
+</div>
+
+<div class="modal-body">
+	<aside class='span'>
+		<div class='sidebar-nav'>
+			<p>
+				Press 'g' for game mode, use cursor keys to move (with 'w' back to walk mode).
+			</p>
+			<b>Navigation:</b>
+			<ul>
+				<li>
+					Walk forward: Left Click/Drag
+				</li>
+				<li>
+					Walk backward: Right Click/Drag or Alt + Left Click
+				</li>
+			</ul>
+		</div>
+	</aside>
+</div>
+
+<div class="modal-footer">
+	<button class="btn" data-dismiss="modal" aria-hidden="true">ปิด</button>
+</div>
