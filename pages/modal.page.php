710909788033aee2dce4b500eed6cdd3f57f1251
pimsuwan <hapztron@gmail.com>
2013-02-07 02:28:58 +0800

    Changed search box position in pop-up page to the same line as header text.

--- pages/modal.page.php
+++ pages/modal.page.php
@@ -2,13 +2,14 @@
 <div id="products" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 	<div class="modal-header">
 		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-		<h3 id="myModalLabel">รายการสินค้า</h3>
-		
-		<div class="input-append">
-			<input type="text" id="p_search">
-			<button type="button" class="btn" onClick="productslist($('#pcid').val(), $('#p_search').val(), 1)">Search</button>
-		</div>
-		
+		<h3 id="myModalLabel">
+			รายการสินค้า
+
+			<div class="input-append pull-right">
+				<input type="text" id="p_search">
+				<button type="button" class="btn" onClick="productslist($('#pcid').val(), $('#p_search').val(), 1)">Search</button>
+			</div>
+		</h3>
 	</div>
 	
 	<div class="modal-body">
@@ -23,11 +24,14 @@
 <div id="cart" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 	<div class="modal-header">
 		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-		<h3 id="myModalLabel">สินค้าในรถเข็น</h3>
-		<div class="input-append">
-			<input type="text" id="c_search">
-			<button type="button" class="btn" onClick="cartlist($('#c_search').val())">Search</button>
-		</div>
+		<h3 id="myModalLabel">
+			สินค้าในรถเข็น
+
+			<div class="input-append pull-right">
+				<input type="text" id="c_search">
+				<button type="button" class="btn" onClick="cartlist($('#c_search').val())">Search</button>
+			</div>
+		</h3>
 	</div>
 	
 	<div class="modal-body">
