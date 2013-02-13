e16bfb42b7b8a33e9354cd26d107a1b19eaa6586
pimsuwan <hapztron@gmail.com>
2013-02-13 17:35:00 +0800

    Changed help text.

--- pages/help.page.php
+++ pages/help.page.php
@@ -1,23 +1,34 @@
 <div class="modal-header">
 	<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-	<h3 id="myModalLabel">Help</h3>
+	<h3 id="myModalLabel">ช่วยเหลือ</h3>
 </div>
 
 <div class="modal-body">
 	<aside class='span'>
 		<div class='sidebar-nav'>
-			<p>
-				Press 'g' for game mode, use cursor keys to move (with 'w' back to walk mode).
-			</p>
-			<b>Navigation:</b>
-			<ul>
-				<li>
-					Walk forward: Left Click/Drag
-				</li>
-				<li>
-					Walk backward: Right Click/Drag or Alt + Left Click
-				</li>
-			</ul>
+			<h1>การควบคุม</h1>
+				<ul>
+					<li>
+						<img src="../styles/images/top.png" />
+						เดินหน้า
+					</li>
+					<li>
+						<img src="../styles/images/left.png" />
+						หันซ้าย
+					</li>
+					<li>
+						<img src="../styles/images/right.png" />
+						หันขวา
+					</li>
+					<li>
+						<img src="../styles/images/down.png" />
+						ถอยหลัง
+					</li>
+					<li>
+						<img src="../styles/images/left_click.png" />
+						หมุน
+					</li>
+				</ul>
 		</div>
 	</aside>
 </div>
